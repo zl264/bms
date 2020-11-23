deff5899de6895b067e97084a15b60d45dedc675
zl264 <2644765104@qq.com>
2020-11-23 23:13:19 +0800

    初始框架的一些注释

--- src/main/java/com/java/bms/common/mapper/CommonMapper.java
+++ src/main/java/com/java/bms/common/mapper/CommonMapper.java
@@ -4,8 +4,18 @@ import com.java.bms.other.DO.UserDO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+/**
+ * 对于普通用户登录注册的数据库访问的控制
+ */
 @Mapper
 public interface CommonMapper {
+
+    /**
+     * 通过用户名和密码来查找输入的用户名密码是否正确
+     * @param username 用户名
+     * @param password 密码
+     * @return 返回按照用户名和密码得到的用户
+     */
     @Select("select * from commonUser where username=#{username} and password=#{password}")
     public UserDO commonLogin(String username, String password);
 }
